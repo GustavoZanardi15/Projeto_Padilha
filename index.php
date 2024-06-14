10a8ce6fcd96636797d13937c2fbe35e5531ab6b
Gustavo Ulian Zanardi <134564393+GustavoZanardi15@users.noreply.github.com>
2024-06-14 15:37:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>PROJ_PADILHA</title>
+  <link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body>
+
+<div id="overlay"></div>
+
+  <div id="popup" class="popup">
+    <h2>ATENÇÃO</h2>
+    <p>Por favor, tome medidas imediatas para proteger sua privacidade e a segurança de suas informações pessoais.</p>
+    <button id="closeBtn">Fechar</button>
+   </div>
+
+<div class="board">
+  <img src="./image/alertAmarelo.png" class="alertAmarelo"> 
+  <!--<img src="./image/caveira.png" class="caveira"> -->
+  <div class="titulo">
+    <h1>AMEAÇA DETECTADA!</h1>
+    <h2>Seus dados foram comprometidos!</h2>
+    <h3>Foi identificado que suas informações foram acessadas de maneira não autorizada. Diante dessa situação,solicitamos que entre em contato urgente com o Departamento de Tecnologia e Informação (T.I.) para que possamos iniciar uma investigação detalhada e implementar medidas de segurança adicionais para proteger nossos bancos de dados.
+    </h3>
+  </div>
+
+  <script src="script.js" defer></script>
+
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const popup = document.getElementById('popup');
+    const overlay = document.getElementById('overlay');
+    const closeBtn = document.getElementById('closeBtn');
+
+    // Mostra o pop-up e o overlay
+    function showPopup() {
+        popup.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+
+    // Fecha o pop-up e o overlay
+    function closePopup() {
+        popup.style.display = 'none';
+        overlay.style.display = 'none'; 
+    }
+
+    closeBtn.addEventListener('click', closePopup);
+
+    setTimeout(showPopup, 3000);
+
+    overlay.addEventListener('click', closePopup);
+});
